5fafd27a8c81d1e53a0529f0dcce724e2e48f9c9
Sabrina Yue Wang <yue.wang1996@gmail.com>
2015-02-18 18:52:50 +0000

    first test compression algorithm

---
+++ cubemu/compression/testimage.py
@@ -0,0 +1,20 @@
+import os
+from PIL import Image
+def compressLoop(infile , times):
+	n=1
+	base, e = os.path.splitext(infile)
+	try:
+			f,e = os.path.splitext(infile)
+			f = (base + str(n))
+			outfile = f + ".jpg"
+			compImg = Image.open(infile)
+			compImg.save(outfile, "JPEG", quality = 10)
+			infile = outfile 
+	except IOError:
+		print "cannot convert", infile
+
+infile = raw_input ("Filename to compress:")
+times = int(input ("Times to process: "))
+compressLoop(infile,times)
+
+
