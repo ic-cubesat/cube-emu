6c3805f3df93b03517d80d1cfbf04667d8ae8808
Thomas Edwards <tom.rb.edwards@gmail.com>
2015-02-25 18:36:47 +0000

    Improved coding style of testimage.py (everything in functions). Added compressed images to .gitignore

--- cubemu/compression/testimage.py
+++ cubemu/compression/testimage.py
@@ -1,20 +1,20 @@
 import os
 from PIL import Image
-def compressLoop(infile , times):
-	n=2
-	base, e = os.path.splitext(infile)
+
+def generateOutfile(infile):
+	base,e = os.path.splitext(infile)
+	return base+"_compressed.jpg"
+
+def compress(infile,outfile,quality=10):
 	try:
-			f,e = os.path.splitext(infile)
-			f = (base + str(n))
-			outfile = f + ".jpg"
-			compImg = Image.open(infile).convert("L")
-			compImg.save(outfile, "JPEG", quality = 10)
-			infile = outfile 
+		compImg = Image.open(infile).convert("L")
+		compImg.save(outfile,"JPEG",quality=quality)
+		print "compressed %s to %s" % (infile,outfile)
 	except IOError:
 		print "cannot convert", infile
 
-infile = raw_input ("Filename to compress:")
-times = int(input ("Times to process: "))
-compressLoop(infile,times)
-
+## Main
+infile = 'testimage.jpg'
+outfile = generateOutfile(infile)
+compress(infile,outfile)
 
