87cd123cfb548da59f61c27c03d623bf7b9f7137
Paul Grigoras <paul.grigoras90@gmail.com>
2015-03-22 16:17:55 +0000

    Minor cleanups in nrf_send and take device on command line.

--- cubemu/communication/nrf_send.py
+++ cubemu/communication/nrf_send.py
@@ -3,77 +3,13 @@ import time
 import signal
 import sys
 
-#test = 146
-data = '0'
-
-ser = serial.Serial('COM16', 57600, timeout = 0.1, interCharTimeout = None)
-print("connected to: " + ser.portstr)
+ser = None
 
 def signal_handler(signal, frame):
     print 'You pressed Ctrl+C!'
     #target.close()
     ser.close()
-    sys.exit(0) 
-
-signal.signal(signal.SIGINT, signal_handler)
-#data = raw_input("Send stuff?")
-#if(data != '0'):
-    #ser.write(data)
-    #ser.write(test)
-time.sleep(1) #needed for some reason...
-
-#f = open('C:\Users\Alex\Dropbox\CubeSat Project\Systems Eng\Comm\NRF24 Code and Library\Python TX code\image.jpeg' , 'rb')
-#contents = f.read()
-#data_len = len(contents)
-#print "Data Length" , data_len
-#print len(contents)
-#contents = contents
-#print len(contents)
-
-#f_2 = open('C:\Users\Alex\Dropbox\CubeSat Project\Systems Eng\Comm\NRF24 Code and Library\Python TX code\image_bytes_sent.jpeg' , 'wb')
-
-def confirm_receive(serial_port,sent_data):
-    i = 0
-    while(1):
-        tdata = serial_port.read(1)
-        if(tdata == sent_data):
-            return True
-        else:
-            i = 1
-            if (i == 1):
-                return False
-            
-def readdata(serial_port,data_size):
-    tdata = serial_port.read(data_size)
-    if(len(tdata) > 0):
-        #print tdata
-        return tdata
-        
-def echo(port, byte):
-    port.write(str(byte))
-    return readdata(port,10)     
-    
-def send_file(port, data):
-    packet_length = 1 #number of bytes to transmit at a time
-    length = int(len(data) / packet_length)
-    for i in range(length): 
-        to_send = data[i*packet_length:(i*packet_length)+packet_length]
-        print i, to_send
-        port.write(to_send)
-        #f_2.write(to_send)
-        #f_2.flush()
-        time.sleep(0.1)
-        if(confirm_receive(port,to_send)):
-            print i, "Packet Confirmed!"
-        else:
-            print i, "Not confirmed!"
-            break
-    if((len(contents) % packet_length) != 0):
-        print "Writing remainder", i, (len(data) % packet_length) , data[i:(i+(len(data) % packet_length))]
-        to_send  = data[i:(i+(len(data) % packet_length))]  
-        port.write(to_send)
-        #f_2.write(to_send)
-        #f_2.flush()  
+    sys.exit(0)
 
 def format_string(data):
     data_array = []
@@ -90,7 +26,7 @@ def format_string(data):
         divider = counter*2
         split = data_len/divider
      #   print "Data" , data_array
-        data_array_c = []       
+        data_array_c = []
         for i in range(divider):
             print "Data Fragement" , data[(split*i):(split*(i+1))] , len(data[(split*i):(split*(i+1))])
             data_array_c.append(data[(split*i):(split*(i+1))])
@@ -154,7 +90,7 @@ def send_string(data_array):
                 print "Received: " , confirm, check(confirm) , check(remove_four(strip_endline(content_copy)))
                 if confirm == str(check(remove_four(strip_endline(content_copy)))):
                     confirmed = 1
-                print "1: " , confirmed,"2: " , confirm, type(confirm)   ,  "3: " ,content, check(content_copy), str(check(remove_four(content_copy)))  
+                print "1: " , confirmed,"2: " , confirm, type(confirm)   ,  "3: " ,content, check(content_copy), str(check(remove_four(content_copy)))
             #time.sleep(2)
     return data_array
 
@@ -168,15 +104,22 @@ def check(data):
         sum += k
     return sum
 
-print "Initial Reading: ", ser.readline()
-time.sleep(0.2)
-data_array = format_string("1234567890")
-print "Data_array" , data_array, check(data_array[0])
+def main():
+  signal.signal(signal.SIGINT, signal_handler)
+  time.sleep(1) #needed for some reason...
 
-#send_string(data_array)
-
-data = raw_input("Send?")
-if(data != '0'):
+  serial_port = sys.argv[1]
+  global ser
+  ser = serial.Serial(serial_port, 57600, timeout = 0.1, interCharTimeout = None)
+  print("connected to: " + ser.portstr)
+  print "Initial Reading: ", ser.readline()
+  time.sleep(0.2)
+  data_array = format_string("1234567890")
+  print "Data_array" , data_array, check(data_array[0])
+  data = raw_input("Send?")
+  if(data != '0'):
     send_string(data_array)
-ser.close()
-print"Done"  
+  ser.close()
+
+if __name__ == '__main__':
+  main()
