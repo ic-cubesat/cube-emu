231f9a8aca8a089384995502c8e8938b0659d0d6
AlviseV <alvise.vianello13@gmail.com>
2015-03-22 13:18:28 +0000

    Implemented dummy request for satellite position

--- cubemu/monitoring/sensors.py
+++ cubemu/monitoring/sensors.py
@@ -1,5 +1,8 @@
 """This module should contain interfaces to all the cubesat sensors."""
 
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 
 def read(sensorName):
     """Read the value of a sensor."""
@@ -16,3 +19,11 @@ def check(sensorName):
 
     print 'Sensor unavailable'
     return None
+
+def gaussian(x, mu, sig):
+    return np.exp(-np.power(x - mu, 2.) / 2 * np.power(sig, 2.))
+
+def getPosition():
+    x = (time.time()%5400)*(360./5400)
+    y = 360*(gaussian(x, 180, 0.001))
+    return x, y
