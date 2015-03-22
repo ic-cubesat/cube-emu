44cdf2c03d706f8013988f236fabaaf0f75a2e35
AlviseV <alvise.vianello13@gmail.com>
2015-03-22 15:23:43 +0000

    Changed Gaussian orbit to sinusoidal orbit

--- cubemu/monitoring/sensors.py
+++ cubemu/monitoring/sensors.py
@@ -1,7 +1,6 @@
 """This module should contain interfaces to all the cubesat sensors."""
 
 import numpy as np
-import matplotlib.pyplot as plt
 import time
 
 def read(sensorName):
@@ -20,10 +19,7 @@ def check(sensorName):
     print 'Sensor unavailable'
     return None
 
-def gaussian(x, mu, sig):
-    return np.exp(-np.power(x - mu, 2.) / 2 * np.power(sig, 2.))
-
-def getPosition():
-    x = (time.time()%5400)*(360./5400)
-    y = 360*(gaussian(x, 180, 0.001))
+def getPosition(t=time.time()%5400):
+    x = t*(360./5400) 
+    y = 180*np.sin(x*np.pi/180) + 180 
     return x, y
